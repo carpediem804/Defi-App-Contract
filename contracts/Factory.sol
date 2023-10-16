9dee89bf87bafc81039fc509c16d723ee38fc150
민태홍 <mintaehong@mintaehong-ui-MacBookPro.local>
2023-10-16 18:20:54 +0900

    [feat] Factory, token function add

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -7,16 +7,21 @@ import './Exchange.sol';
 contract Factory{
 
     mapping(address => address) tokenToExchangeAddress ;
+    mapping(address => address) ExchangeToTokenAddress ;
 
 
     function createExchange(address _token)public returns(address) {
         Exchange exchange = new Exchange(_token);
         tokenToExchangeAddress[_token] = address(exchange);
-
+        ExchangeToTokenAddress[address(exchange)] = _token;
         return address(exchange);
     }
 
     function getExchangeAddress (address _token) public view returns( address){
         return tokenToExchangeAddress[_token];
+    }
+
+    function getToken (address _exchange) public view returns( address){
+        return ExchangeToTokenAddress[_exchange];
     }
 }
--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,3 +1,4 @@
+//SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
@@ -7,7 +8,10 @@ contract Token is ERC20 {
         string memory name,
         string memory symbol,
         uint256 initialSupply
-    ) ERC20(name, symbol) {
-        _mint(msg.sender, initialSupply);
+    ) ERC20(name, symbol) {}
+
+    function mint(address to, uint256 _amount) public {
+        _mint(to, _amount);
     }
+       
 }
